1b299062d1da364937c5d9c371e1d15922463c07
Timea2 <timeapodrug@gmail.com>
2023-07-04 18:01:00 +0200

    Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
+
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -16,7 +22,50 @@ const Header = () => {
         <Link href="/">
           <img src="/logo.png" className="w-[40px] md:w-[60px]" alt="logo" />
         </Link>
+
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+          />
+        )}
+
+        <div className="flex items-center gap-2 text-black">
+          {/* heart icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px] " />
+            <div className="h-[14px] md:h-[18px] min-w-[14px]  md:min-w-[18px]  rounded-full bg-red-600 absolute top-1 left-5  md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              51
+            </div>
+          </div>
+          {/* heart icon end */}
+          {/* shopping cart icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px] " />
+            <div className="h-[14px] md:h-[18px] min-w-[14px]  md:min-w-[18px]  rounded-full bg-red-600 absolute top-1 left-5  md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>
+          </div>
+          {/* shopping cart icon end */}
+          {/* Mobile icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+          {/* Mobile icon end */}
+        </div>
       </Wrapper>
     </header>
   );
